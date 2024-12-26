22b052c4cf9e1f6065ca5fe2b0cf21dacacaf9fd
furqan-hue <furqantasiya@gmail.com>
2024-12-26 12:17:03 +0000

    container config done

---
+++ function/basic.js
@@ -0,0 +1,32 @@
+// function sum(a, b){
+//     return a + b;
+// }
+// function subtraction(a, b){
+//     return a - b;
+// }
+// function multi(a, b){
+//     return a * b;
+// }
+// function divid(a, b){
+//     return a/b;
+// }
+// function mod(a, b){
+//     return a % b;
+// }
+// console.log(sum(5,5));
+// console.log(subtraction(5,5));
+// console.log(multi(5,5));
+// console.log(divid(10,2));
+// console.log(mod(15,2));
+
+
+
+
+
+
+
+function greet(name){
+    console.log("hello " + name + ", welcome to thapa");
+}
+greet("ram");
+greet("tasiya");
